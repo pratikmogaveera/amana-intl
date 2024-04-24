50fdb91098597ac699a5e69222fab2066887197d
zafir3 <pratikmogaveera@gmail.com>
2024-04-24 13:52:46 +0530

    Changed Button color on HomeProducts

--- src/components/HomeProducts.tsx
+++ src/components/HomeProducts.tsx
@@ -39,7 +39,7 @@ const HomeProducts = () => {
                                             <CardFooter>
                                                 <Link
                                                     href={item.href}
-                                                    className={buttonVariants()}
+                                                    className='text-primary-foreground bg-company-secondary hover:bg-company-secondary/90 h-10 px-4 py-2 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50'
                                                 >
                                                     Read More
                                                 </Link>
